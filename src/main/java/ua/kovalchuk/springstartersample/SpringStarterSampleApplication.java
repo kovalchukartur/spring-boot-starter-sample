c462503a18557b253607f73467c1aec025d03f70
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-12-09 00:58:23 +0200

    Init

---
+++ src/main/java/ua/kovalchuk/springstartersample/SpringStarterSampleApplication.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.springstartersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStarterSampleApplication.class, args);
+    }
+
+}
